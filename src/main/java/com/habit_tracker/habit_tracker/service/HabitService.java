0a1165bb300e95d07c5b6eedce1684d5d1290840
Mcurci95 <mcurci95@gmail.com>
2020-03-31 23:06:44 -0700

    Service interface and implementation of add and get habits

--- src/main/java/com/habit_tracker/habit_tracker/service/HabitService.java
+++ src/main/java/com/habit_tracker/habit_tracker/service/HabitService.java
@@ -1,4 +1,14 @@
 package com.habit_tracker.habit_tracker.service;
 
+
+import com.habit_tracker.habit_tracker.models.Habit;
+import com.habit_tracker.habit_tracker.models.HabitData;
+
 public interface HabitService {
+
+    void addHabit(Habit habit);
+    void removeHabit(int id);
+    Habit getHabit(int id);
+    void updateHabit(Habit habit);
+    HabitData getData();
 }
--- src/main/java/com/habit_tracker/habit_tracker/service/HabitServiceImpl.java
+++ src/main/java/com/habit_tracker/habit_tracker/service/HabitServiceImpl.java
@@ -1,4 +1,36 @@
 package com.habit_tracker.habit_tracker.service;
 
-public class HabitServiceImpl {
+import com.habit_tracker.habit_tracker.models.Habit;
+import com.habit_tracker.habit_tracker.models.HabitData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HabitServiceImpl implements HabitService {
+
+    public HabitData habitData = new HabitData();
+
+    @Override
+    public void addHabit(Habit habit) {
+        habitData.addHabit(habit);
+    }
+
+    @Override
+    public void removeHabit(int id) {
+
+    }
+
+    @Override
+    public Habit getHabit(int id) {
+        return habitData.getHabit(id);
+    }
+
+    @Override
+    public void updateHabit(Habit habit) {
+
+    }
+
+    @Override
+    public HabitData getData() {
+        return null;
+    }
 }
