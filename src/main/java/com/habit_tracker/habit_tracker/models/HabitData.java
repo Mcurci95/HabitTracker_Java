9f7526994945576d8a415f804a02bfefe107fd7c
Mcurci95 <mcurci95@gmail.com>
2020-03-31 22:59:16 -0700

    Utilizing lombok

--- src/main/java/com/habit_tracker/habit_tracker/models/HabitData.java
+++ src/main/java/com/habit_tracker/habit_tracker/models/HabitData.java
@@ -1,7 +1,38 @@
 package com.habit_tracker.habit_tracker.models;
 
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class HabitData {
 
     // In memory data for testing
 
+    public static int idValue;
+
+    public final List<Habit> habits = new ArrayList<>();
+
+    public HabitData() {
+    }
+
+    public List<Habit> getHabits() {
+        return Collections.unmodifiableList(habits);
+    }
+
+    public void addHabit(@NonNull Habit habit) {
+        habit.setId(idValue);
+        habits.add(habit);
+        idValue++;
+    }
+
+    public Habit getHabit(int id) {
+        for (Habit habit : habits) {
+            if (habit.getId() == id) {
+                return habit;
+            }
+        }
+        return null;
+    }
 }
