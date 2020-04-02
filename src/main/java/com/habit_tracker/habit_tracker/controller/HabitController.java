2ec7777f9ed515c814e6dc16282b9e69fde6ad54
mcurci95 <mcurci95@gmail.com>
2020-04-01 23:15:02 -0700

    Get all habits

--- src/main/java/com/habit_tracker/habit_tracker/controller/HabitController.java
+++ src/main/java/com/habit_tracker/habit_tracker/controller/HabitController.java
@@ -1,4 +1,24 @@
 package com.habit_tracker.habit_tracker.controller;
 
+import com.habit_tracker.habit_tracker.models.Habit;
+import com.habit_tracker.habit_tracker.models.HabitData;
+import com.habit_tracker.habit_tracker.service.HabitService;
+import com.habit_tracker.habit_tracker.service.HabitServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
 public class HabitController {
+
+    private static final HabitService service = new HabitServiceImpl();
+
+    @GetMapping("/")
+    public HabitData getAll() {
+        log.info("calling get mapping");
+        return service.getData();
+    }
 }
