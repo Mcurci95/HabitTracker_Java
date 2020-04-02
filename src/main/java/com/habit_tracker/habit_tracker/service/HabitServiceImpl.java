4d832a5793e587a6692ea718965304d1f613cd46
mcurci95 <mcurci95@gmail.com>
2020-04-01 23:28:13 -0700

    get data function

--- src/main/java/com/habit_tracker/habit_tracker/service/HabitServiceImpl.java
+++ src/main/java/com/habit_tracker/habit_tracker/service/HabitServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HabitServiceImpl implements HabitService {
 
-    
+
     public HabitData habitData = new HabitData();
 
     @Override
