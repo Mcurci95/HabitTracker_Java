a2b24bc045794d7188f7a888d42a8fec81827763
mcurci95 <mcurci95@gmail.com>
2020-04-03 23:36:52 -0700

    Habit table

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as ReactBootstrap from 'react-bootstrap';
 
 class App extends React.Component {
 
@@ -13,50 +14,60 @@ class App extends React.Component {
         };
     }
 
-
-
      async componentDidMount() {
         const response = await fetch("http://localhost:8080");
         const body = await response.json();
-        console.log(body);
         this.setState({habits: body, isLoading: false});
+    }
+
+    renderHabit(habit) {
+        return (
+            <tr key={habit.id}>
+                <td>{habit.id}</td>
+                <td>{habit.title}</td>
+                <td>{habit.minutes}</td>
+                <td>{habit.location}</td>
+                <td>Details</td>
+                <td>Edit link</td>
+            </tr>
+        )
     }
 
     render() {
         const {habits, isLoading} = this.state;
+        let habitList = [];
 
         if (isLoading) {
             return <p>Loading...</p>
         }
-        let habitList = [];
 
         for(let i=0; i < habits.habits.length; i++) {
             console.log(habits.habits[i]);
             habitList.push(habits.habits[i]);
         }
 
-
-
         return (
-            <div className="App">
-                <header className="App-header">
+
                     <div className="App-intro">
                         <h2>Habits:</h2>
-                        {habitList.map(habit =>
-                            <div>
-                                {habit.id} -
-                                {habit.title} -
-                                {habit.details} -
-                                {habit.minutes} mins
+                        <ReactBootstrap.Table striped bordered hover>
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Habit</th>
+                                <th>Minutes</th>
+                                <th>Location</th>
+                                <th>Details</th>
+                                <th>Edit</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {habitList.map(habit => this.renderHabit(habit))}
+                            </tbody>
+                        </ReactBootstrap.Table>
 
-                            </div>
-                        )
-                        }
-
-                        <div></div>
                     </div>
-                </header>
-            </div>
+
         );
     }
 }
