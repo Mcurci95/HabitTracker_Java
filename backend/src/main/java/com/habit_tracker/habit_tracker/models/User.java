79d5d7dac940398f24ce60436335e25ffc9e4340
mcurci95 <mcurci95@gmail.com>
2020-06-22 21:56:46 -0700

    User model

--- backend/src/main/java/com/habit_tracker/habit_tracker/models/User.java
+++ backend/src/main/java/com/habit_tracker/habit_tracker/models/User.java
@@ -1,4 +1,12 @@
 package com.habit_tracker.habit_tracker.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+    private int id;
+    private String username;
+    private String password;
 }
